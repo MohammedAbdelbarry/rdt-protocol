4fe4f17c29fb0683b38f653f727d20164fd5eb57
Mohammed Abdelbarry <m7md337@gmail.com>
2018-12-15 22:36:23 +0200

    Optimize imports

--- src/main/java/edu/csed/networks/rdt/protocol/strategy/StopAndWaitStrategy.java
+++ src/main/java/edu/csed/networks/rdt/protocol/strategy/StopAndWaitStrategy.java
@@ -1,25 +1,7 @@
 package edu.csed.networks.rdt.protocol.strategy;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 public class StopAndWaitStrategy extends SelectiveRepeatStrategy {
     public StopAndWaitStrategy() {
         super(1);
     }
-//
-//    @Override
-//    public void acceptAck(long seqNo) {
-//        unackedPackets.remove(seqNo);
-//        if (seqNo == windowBase) {
-//            windowBase++;
-//        }
-//    }
-//
-//    @Override
-//    public Collection<Long> packetTimedOut(long seqNo) {
-//        return new ArrayList<>(Collections.singletonList(seqNo));
-//    }
 }
